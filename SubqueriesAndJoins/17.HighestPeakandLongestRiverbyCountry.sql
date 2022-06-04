3fee1b67194ed6eb85c6c5649df28477bc6905b3
Иксан <iksan168@abv.bg>
2022-06-05 00:22:25 +0300

    SomeExercise

---
+++ SubqueriesAndJoins/17.HighestPeakandLongestRiverbyCountry.sql
@@ -0,0 +1,13 @@
+SELECT TOP 5
+c.CountryName
+,MAX([p].[Elevation]) AS [HighestPeakEleveation]
+,MAX(r.[Length]) AS [LongestRiverLength]
+FROM 
+Countries AS c
+LEFT JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
+LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
+LEFT JOIN Peaks AS p ON m.Id = p.MountainId
+GROUP BY c.CountryName
+ORDER BY [HighestPeakEleveation] DESC, [LongestRiverLength] DESC, c.CountryName
