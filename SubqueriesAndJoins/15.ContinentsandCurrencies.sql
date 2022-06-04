d311f6538377c91fe474aee0f76d893b741d0b9d
Иксан <iksan168@abv.bg>
2022-06-04 20:14:55 +0300

    SomeExercise

---
+++ SubqueriesAndJoins/15.ContinentsandCurrencies.sql
@@ -0,0 +1,23 @@
+SELECT
+[ContinentCode]
+,[CurrencyCode]
+,[CurrencyUsage]
+FROM
+(
+	SELECT *,
+	DENSE_RANK() OVER (PARTITION BY [ContinentCode] ORDER BY [CurrencyUsage] DESC) AS [CurrencyRank]
+	FROM
+	(
+		SELECT
+		co.ContinentCode AS [ContinentCode]
+		,c.CurrencyCode AS [CurrencyCode]
+		,COUNT(c.CurrencyCode) AS [CurrencyUsage]
+		FROM
+		Continents AS co
+		LEFT JOIN Countries AS c ON c.ContinentCode = co.ContinentCode
+		GROUP BY co.ContinentCode, c.CurrencyCode
+	) AS [CurrencyUsageQuery]
+	WHERE [CurrencyUsage] > 1
+) AS [CurrencyRankingQuery]
+WHERE [CurrencyRank] = 1
+ORDER BY [ContinentCode]
