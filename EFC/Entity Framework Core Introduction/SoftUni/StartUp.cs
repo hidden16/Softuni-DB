5f1920615aeb35b06cdf6b7f2e0690b4b4785052
Иксан <iksan168@abv.bg>
2022-06-29 17:30:38 +0300

    EFCExercise

--- EFC/Entity Framework Core Introduction/SoftUni/StartUp.cs
+++ EFC/Entity Framework Core Introduction/SoftUni/StartUp.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace SoftUni
 {
-    internal class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SoftUniContext context = new SoftUniContext();
+            string result = GetEmployeesFullInformation(context);
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in context.Employees.OrderBy(x => x.EmployeeId))
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return sb.ToString();
         }
     }
 }
