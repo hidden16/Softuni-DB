cf920aa52833b664024bb5c8d7321371c49d0b09
Иксан <iksan168@abv.bg>
2022-06-29 17:03:35 +0300

    EFCExercise

---
+++ EFC/Entity Framework Core Introduction/SoftUni/Models/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SoftUni.Models;
+namespace SoftUni
+{
+    public partial class Address
+    {
+        public Address()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        [Key]
+        [Column("AddressID")]
+        public int AddressId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string AddressText { get; set; }
+        [Column("TownID")]
+        public int? TownId { get; set; }
+
+        [ForeignKey(nameof(TownId))]
+        [InverseProperty(nameof(SoftUni.Town.Addresses))]
+        public virtual Town Town { get; set; }
+        [InverseProperty("Address")]
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ EFC/Entity Framework Core Introduction/SoftUni/Models/EmployeeProject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftUni.Models
+{
+    public partial class EmployeeProject
+    {
+        [Key]
+        [Column("EmployeeID")]
+        public int EmployeeId { get; set; }
+        [Key]
+        [Column("ProjectID")]
+        public int ProjectId { get; set; }
+
+        [ForeignKey(nameof(EmployeeId))]
+        [InverseProperty(nameof(SoftUni.Employee.EmployeesProjects))]
+        public virtual Employee Employee { get; set; }
+        [ForeignKey(nameof(ProjectId))]
+        [InverseProperty(nameof(SoftUni.Project.EmployeesProjects))]
+        public virtual Project Project { get; set; }
+    }
+}
