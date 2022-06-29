67eefc97f5e29151d6511ccfaed02bd3ee3f63d1
Иксан <iksan168@abv.bg>
2022-06-29 17:34:45 +0300

    EFCExercise

--- EFC/Entity Framework Core Introduction/SoftUni/StartUp.cs
+++ EFC/Entity Framework Core Introduction/SoftUni/StartUp.cs
@@ -11,6 +11,7 @@ namespace SoftUni
         {
             SoftUniContext context = new SoftUniContext();
             string result = GetEmployeesFullInformation(context);
+            result = GetEmployeesWithSalaryOver50000(context);
             Console.WriteLine(result);
         }
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -21,6 +22,15 @@ namespace SoftUni
                 sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
             }
             return sb.ToString();
+        }
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in context.Employees.Where(x=>x.Salary>50_000).OrderBy(x=>x.FirstName))
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
         }
     }
 }
