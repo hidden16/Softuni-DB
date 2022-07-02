5a5df704b2d58cece145a1ee17b90b3916179789
Иксан <iksan168@abv.bg>
2022-07-02 19:13:12 +0300

    EntityRelationsExercise

---
+++ EFC/Entity Relations/StudentSystem/Data/Models/ContentType.cs
@@ -0,0 +1,10 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    public enum ContentType
+    {
+        Application = 1,
+        Pdf = 2,
+        Zip = 3
+    }
+    //o ContentType(enum – can be Application, Pdf or Zip)
+}
---
+++ EFC/Entity Relations/StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        private const string connectionString = @"Server=IKSAN\SQLEXPRESS;Database=StudentSystem;Integrated Security=true;";
+        public StudentSystemContext()
+        {
+
+        }
+        public StudentSystemContext(DbContextOptions<StudentSystemContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(x=>
+            {
+                x.HasKey(x => new { x.StudentId, x.CourseId });
+            });
+                
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
