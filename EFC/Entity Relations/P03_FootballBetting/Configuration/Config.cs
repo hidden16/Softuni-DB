5fc4367b2297f2d4489c7818aef60278f7f2141e
Иксан <iksan168@abv.bg>
2022-07-03 13:41:13 +0300

    EntityRelationsExercise

---
+++ EFC/Entity Relations/P03_FootballBetting/Configuration/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Configuration
+{
+    public static class Config
+    {
+        public const string ConnectionString = @"Server=IKSAN\SQLEXPRESS;Database=FootballBetting;Integrated Security=True;";
+    }
+}
---
+++ EFC/Entity Relations/P03_FootballBetting/Data/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            Towns = new HashSet<Town>();
+        }
+        [Key]
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Town> Towns { get; set; }
+    }
+}
