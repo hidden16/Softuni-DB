2fef2bc86319164427457e89ca60aaedd67214e8
Иксан <iksan168@abv.bg>
2022-07-06 20:34:11 +0300

    LINQExercise

---
+++ EFC/LINQ/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,67 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongsPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Song>(s =>
+            {
+                s.HasOne(x => x.Writer)
+                .WithMany(x => x.Songs)
+                .HasForeignKey(x=>x.WriterId);
+
+                s.HasOne(x => x.Album)
+                .WithMany(x => x.Songs)
+                .HasForeignKey(x => x.AlbumId);
+            });
+
+            builder.Entity<Album>(a =>
+            {
+                a.HasOne(x => x.Producer)
+                .WithMany(x => x.Albums)
+                .HasForeignKey(x => x.ProducerId);
+            });
+
+            builder.Entity<SongPerformer>(w =>
+            {
+                w.HasKey(x => new { x.SongId, x.PerformerId });
+
+                w.HasOne(x => x.Song)
+                .WithMany(x => x.SongPerformers)
+                .HasForeignKey(x => x.SongId);
+
+                w.HasOne(x => x.Performer)
+                .WithMany(x => x.PerformerSongs)
+                .HasForeignKey(x=>x.PerformerId);
+
+            });
+        }
+    }
+}
