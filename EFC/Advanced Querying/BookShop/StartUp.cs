33e76caebeffd583d469dab105bc4e9b6c09b118
Иксан <iksan168@abv.bg>
2022-07-10 19:31:25 +0300

    AdvancedQueryingExercise

---
+++ EFC/Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            var input = Console.ReadLine();
+            var commands = GetBooksByAgeRestriction(db, input);
+            Console.WriteLine(commands);
+        }
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+            command = command.ToLower();
+            command = char.ToUpper(command[0]) + command.Substring(1);
+            foreach (var titles in context.Books
+                                    .Where(x => x.AgeRestriction == (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command))
+                                    .OrderBy(x=>x.Title)
+                                    .Select(x => x.Title))
+            {
+                sb.AppendLine(titles);
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
