460fb8e3db7279aa6484a1c9147a0a2ec79c63e1
Иксан <iksan168@abv.bg>
2022-07-25 13:58:22 +0200

    ExamPrep

---
+++ EFC/Exam Preparation/BookShop/Data/Models/Constants/GlobalConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Data.Models.Constants
+{
+    public static class GlobalConstant
+    {
+        public const int AUTHOR_FIRST_NAME_MIN_LENGTH = 3;
+        public const int AUTHOR_FIRST_NAME_MAX_LENGTH = 30;
+        public const int AUTHOR_LAST_NAME_MIN_LENGTH = 3;
+        public const int AUTHOR_LAST_NAME_MAX_LENGTH = 30;
+    }
+}
