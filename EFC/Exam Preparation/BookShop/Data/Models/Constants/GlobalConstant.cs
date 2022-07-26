5879e267a25bcba93a15ee2b9c4aa18bc16df361
Иксан <iksan168@abv.bg>
2022-07-26 14:04:02 +0200

    ExamPrep

--- EFC/Exam Preparation/BookShop/Data/Models/Constants/GlobalConstant.cs
+++ EFC/Exam Preparation/BookShop/Data/Models/Constants/GlobalConstant.cs
@@ -6,5 +6,9 @@
         public const int AUTHOR_FIRST_NAME_MAX_LENGTH = 30;
         public const int AUTHOR_LAST_NAME_MIN_LENGTH = 3;
         public const int AUTHOR_LAST_NAME_MAX_LENGTH = 30;
+        public const int BOOK_NAME_MIN_LENGTH = 3;
+        public const int BOOK_NAME_MAX_LENGTH = 30;
+        public const int BOOK_PAGES_MIN_LENGTH = 3;
+        public const int BOOK_PAGES_MAX_LENGTH = 30;
     }
 }
