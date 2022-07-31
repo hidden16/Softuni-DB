f57e99380cad8bbe0684c67e8c4d578867e5892d
Иксан <iksan168@abv.bg>
2022-07-31 17:36:59 +0300

    Exam Preparation

--- EFC/Exam Preparation/C#DBAdvancedExam-04Dec2021/Theatre/DataProcessor/Deserializer.cs
+++ EFC/Exam Preparation/C#DBAdvancedExam-04Dec2021/Theatre/DataProcessor/Deserializer.cs
@@ -1,9 +1,18 @@
 ﻿namespace Theatre.DataProcessor
 {
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
     using Theatre.Data;
+    using Theatre.Data.Models;
+    using Theatre.Data.Models.Enums;
+    using Theatre.DataProcessor.ImportDto;
 
     public class Deserializer
     {
@@ -20,17 +29,116 @@
 
         public static string ImportPlays(TheatreContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            var playsDto = XmlDeserializer<PlayImport>("Plays", xmlString);
+            StringBuilder sb = new StringBuilder();
+            List<Play> plays = new List<Play>();
+            foreach (var play in playsDto)
+            {
+                if (!IsValid(play))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var genreCheck = new Genre[]
+                {
+                    Genre.Comedy,
+                    Genre.Drama,
+                    Genre.Musical,
+                    Genre.Romance
+                };
+                if (!genreCheck.Any(x => x.ToString() == play.Genre))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                if (TimeSpan.Parse(play.Duration).Hours < 1)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                sb.AppendLine(string.Format(SuccessfulImportPlay, play.Title, play.Genre, play.Rating));
+                plays.Add(new Play()
+                {
+                    Genre = (Genre)Enum.Parse(typeof(Genre), play.Genre),
+                    Description = play.Description,
+                    Duration = TimeSpan.ParseExact(play.Duration, "c", CultureInfo.InvariantCulture),
+                    Rating = play.Rating,
+                    Screenwriter = play.Screenwriter,
+                    Title = play.Title
+                });
+            }
+            context.Plays.AddRange(plays);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportCasts(TheatreContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            var castsDto = XmlDeserializer<CastImport>("Casts", xmlString);
+            StringBuilder sb = new StringBuilder();
+            List<Cast> casts = new List<Cast>();
+            foreach (var cast in castsDto)
+            {
+                if (!IsValid(cast))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var mainOrLesser = cast.IsMainCharacter ? "main" : "lesser";
+                sb.AppendLine(string.Format(SuccessfulImportActor, cast.FullName, mainOrLesser));
+                casts.Add(new Cast()
+                {
+                    FullName = cast.FullName,
+                    IsMainCharacter = cast.IsMainCharacter,
+                    PhoneNumber = cast.PhoneNumber,
+                    PlayId = cast.PlayId
+                });
+            }
+            context.Casts.AddRange(casts);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportTtheatersTickets(TheatreContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var theatreDto = JsonConvert.DeserializeObject<TheatreImport[]>(jsonString);
+            StringBuilder sb = new StringBuilder();
+            List<Theatre> theatres = new List<Theatre>();
+            foreach (var theatre in theatreDto)
+            {
+                if (!IsValid(theatre))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                List<Ticket> tickets = new List<Ticket>();
+                foreach (var ticket in theatre.Tickets)
+                {
+                    if (!IsValid(ticket))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    tickets.Add(new Ticket()
+                    {
+                        RowNumber = ticket.RowNumber,
+                        PlayId = ticket.PlayId,
+                        Price = ticket.Price,
+                    });
+                }
+                Theatre theatreToAdd = new Theatre()
+                {
+                    Director = theatre.Director,
+                    Name = theatre.Name,
+                    NumberOfHalls = theatre.NumberOfHalls,
+                    Tickets = tickets
+                };
+                sb.AppendLine(string.Format(SuccessfulImportTheatre, theatre.Name, tickets.Count));
+                theatres.Add(theatreToAdd);
+            }
+            context.Theatres.AddRange(theatres);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
         }
 
 
@@ -41,6 +149,15 @@
 
             var result = Validator.TryValidateObject(obj, validator, validationRes, true);
             return result;
+        }
+        private static T[] XmlDeserializer<T>(string rootAttribute, string stringXml)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootAttribute);
+            XmlSerializer serializer = new XmlSerializer(typeof(T[]), root);
+            var reader = new StringReader(stringXml);
+
+            var deserialized = serializer.Deserialize(reader) as T[];
+            return deserialized;
         }
     }
 }
---
+++ EFC/Exam Preparation/C#DBAdvancedExam-04Dec2021/Theatre/DataProcessor/ImportDto/CastImport.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Cast")]
+    public class CastImport
+    {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string FullName { get; set; }
+        [Required]
+        public bool IsMainCharacter { get; set; }
+        [Required]
+        [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int PlayId { get; set; }
+    }
+}
