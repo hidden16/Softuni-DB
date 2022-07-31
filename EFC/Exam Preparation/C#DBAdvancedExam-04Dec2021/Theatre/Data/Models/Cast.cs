860ddb6f16e12f7c15aecef84f2ca2b685f250e3
Иксан <iksan168@abv.bg>
2022-07-31 16:42:46 +0300

    Exam Preparation

---
+++ EFC/Exam Preparation/C#DBAdvancedExam-04Dec2021/Theatre/Data/Models/Cast.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Theatre.Data.Models
+{
+    public class Cast
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string FullName { get; set; }
+        [Required]
+        public bool IsMainCharacter { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int PlayId { get; set; }
+        public Play Play { get; set; }
+    }
+}
