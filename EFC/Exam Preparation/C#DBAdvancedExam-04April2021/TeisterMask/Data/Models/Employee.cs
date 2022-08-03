4b1d996669318c1e322f5cf37488a525fe39be4c
Иксан <iksan168@abv.bg>
2022-08-03 21:02:08 +0300

    Exam Preparation

---
+++ EFC/Exam Preparation/C#DBAdvancedExam-04April2021/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)] 
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
